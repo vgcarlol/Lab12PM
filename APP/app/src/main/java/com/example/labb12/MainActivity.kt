33d7a8aa2e237e0c187dfabf86afa095f21d0cb8
Carlos Valladares <val221164@uvg.edu.gt>
2023-11-07 09:57:33 -0600

    Added responsive

--- APP/app/src/main/java/com/example/labb12/MainActivity.kt
+++ APP/app/src/main/java/com/example/labb12/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.labb12
 
+
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
+    private val myDataset = arrayOf("Element 1", "Element 2", "Element 3", "Element 4", "Element 5") // Ejemplo de datos
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.adapter = MyAdapter(myDataset)
+
         configureLayoutManager()
     }
 
@@ -24,12 +31,31 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun configureLayoutManager() {
+        val isTablet = resources.getBoolean(R.bool.isTablet)
         val orientation = resources.configuration.orientation
-        recyclerView.layoutManager = if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            // Usamos 2 columnas en orientación horizontal
-            GridLayoutManager(this, 2)
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE || isTablet) {
+            // Aumentamos el número de columnas para tablets o paisaje
+            recyclerView.layoutManager = GridLayoutManager(this, if (isTablet) 3 else 2)
         } else {
-            LinearLayoutManager(this)
+            recyclerView.layoutManager = LinearLayoutManager(this)
         }
     }
+}
+
+class MyAdapter(private val dataset: Array<String>) :
+    RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val textView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false) as TextView
+        return MyViewHolder(textView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.textView.text = dataset[position]
+    }
+
+    override fun getItemCount() = dataset.size
 }
